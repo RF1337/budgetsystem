f635519be7aca1e409e55e999d15771143dc03eb
Rasmus Koudal Ferst <rasm427u@zbc.dk>
2025-05-08 18:45:54 +0200

    Added data layer for connection with db and models

---
+++ budgetsystem/budgetsystem/budgetsystem.Shared/Data/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budgetsystem.Shared.Data
+{
+    internal class DbContext
+    {
+    }
+}
