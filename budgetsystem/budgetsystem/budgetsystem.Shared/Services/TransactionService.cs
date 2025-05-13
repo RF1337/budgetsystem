a1d75e85fcb3f3d75e4c82640f53885daa6fc6dd
rf1337 <rasmus.ferst@gmail.com>
2025-05-13 14:13:14 +0200

    Added Update and Delete methods

--- budgetsystem/budgetsystem/budgetsystem.Shared/Services/TransactionService.cs
+++ budgetsystem/budgetsystem/budgetsystem.Shared/Services/TransactionService.cs
@@ -20,11 +20,43 @@ namespace budgetsystem.Shared.Services
                 .ToListAsync();
         }
 
+        // Create a transaction
+
         public async Task AddAsync(Transaction transaction)
         {
             _db.Transactions.Add(transaction);
             await _db.SaveChangesAsync();
         }
+
+        // Update a transaction
+        public async Task UpdateAsync(Transaction updatedTransaction)
+        {
+            var existing = await _db.Transactions.FindAsync(updatedTransaction.TransactionID);
+
+            if (existing is not null)
+            {
+                existing.Description = updatedTransaction.Description;
+                existing.Amount = updatedTransaction.Amount;
+                existing.CategoryId = updatedTransaction.CategoryId;
+                existing.Type = updatedTransaction.Type;
+                existing.TransactionDate = updatedTransaction.TransactionDate;
+                existing.UserID = updatedTransaction.UserID;
+
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        // Delete a transaction
+        public async Task DeleteAsync(int transactionId)
+        {
+            var transaction = await _db.Transactions.FindAsync(transactionId);
+            if (transaction != null)
+            {
+                _db.Transactions.Remove(transaction);
+                await _db.SaveChangesAsync();
+            }
+        }
+
 
         public async Task<decimal> GetTotalExpensesAsync()
         {
